db4b7757b8be9f4111e743bb5294f13d76633a46
Elise <Elise@mac.mynetworksettings.com>
2025-04-28 21:39:39 -0400

    first deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
-import AccessibleComponent from './pages/AccessibleComponent.tsx';
 import Personas from './pages/Personas.tsx';
-import ResponsiveRedesign from './pages/ResponsiveRedesign.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,26 +12,14 @@ const root = ReactDOM.createRoot(
 
 const router = createBrowserRouter([
   {
-      path: '/InteractionDesign-New/',
+      path: '/Personas/',
       element: <App/>,
       // errorElement: <NotFound/>,
       children: [
         {
-          path: '/InteractionDesign-New/',
-          element: <AccessibleComponent/>
-        },
-        {
-          path: '/InteractionDesign-New/AccessibleComponent',
-          element: <AccessibleComponent/>
-        },
-        {
-          path:'/InteractionDesign-New/PersonasStoryboarding',
+          path:'/Personas/',
           element: <Personas/>
         },
-        {
-          path:'/InteractionDesign-New/ResponsiveRedesign',
-          element: <ResponsiveRedesign/>
-        }
       ]
   }
 ])
